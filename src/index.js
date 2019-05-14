7225b7d333898003f2120d992c527193681edb3f
Nate <1239242238@qq.com>
2019-05-14 17:09:31 +0800

    feat: release 2.8.2-beta.2

--- src/index.js
+++ src/index.js
@@ -184,7 +184,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.8.2-beta.1',
+  version: '2.8.2-beta.2',
   locale: locale.use,
   i18n: locale.i18n,
   install,
